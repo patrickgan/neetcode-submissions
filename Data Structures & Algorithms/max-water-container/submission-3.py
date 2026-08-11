6f93670e8196d6a3658a338f0ba42f9fe1560025
Patrick Gan <patrickgan@users.noreply.github.com>
2026-08-10 23:39:53 -0700

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.py
@@ -0,0 +1,34 @@
+"""
+Formula:
+area_(i,j) = min(height_i, height_j) * (j - i)
+
+Can't just iterate through all possible combinations, as that would be O(n^2) time.
+How to decide between which combination to go to?
+Maybe we keep going until we reach a higher height, then calculate from there,
+as any less-extreme index with the same height will only return a smaller area.
+
+How do we decide between moving the head and moving the tail?
+
+1,1,1,1,1,100,100,1,1,1,1
+1,1,1,1,100,1,1,1,1,100
+"""
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        head = 0
+        tail = len(heights) - 1
+        result = -1
+        while head < tail:
+            result = max(result, self.area(heights, head, tail))
+            if heights[head] < heights[tail]:
+                head += 1
+            elif heights[head] > heights[tail]:
+                tail -= 1
+            else:
+                head += 1
+                tail -= 1
+        return result
+
+    def area(self, heights, i, j):
+        assert j > i, "Second height must come after first height"
+        return (j - i) * min(heights[i], heights[j])
